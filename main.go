1ce278f6864b789146522f265594e28d0805936b
Reagan Iwadha <reaganiwadha@protonmail.com>
2019-12-05 21:39:24 +0700

    Init

---
+++ main.go
@@ -0,0 +1,36 @@
+// | melun/reaganiwadha/Regan Iwadha Google Code In Submission |
+
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+var pathMap = make(map[string]string)
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	path = strings.TrimPrefix(path, "/")
+	if path == "" {
+		w.Write([]byte("Welcome to my link shortener!, type some predefined path for redirections"))
+	} else if pathMap[path] == "" {
+		w.Write([]byte("Well that sucks... But there is no predefined path for that"))
+	} else {
+		http.Redirect(w, r, pathMap[path], 301)
+	}
+	print(path)
+}
+
+func main() {
+	//Declare your maps here
+	pathMap["google"] = "https://google.com"
+	pathMap["go"] = "https://golang.org"
+	pathMap["github"] = "https://github.com/reaganiwadha"
+	pathMap["gci"] = "https://codein.withgoogle.com/"
+
+	http.HandleFunc("/", redirect)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
